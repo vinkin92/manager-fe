908c9f4eb8bd0da360af78ab46c730015ad9ca98
vinkim <928287706@qq.com>
2022-06-11 17:54:44 +0800

    封装登录请求接口

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import ElementPlus from "element-plus";
 import "element-plus/lib/theme-chalk/index.css";
 import request from "./utils/request.js";
 import storage from "./utils/storage.js"
+import api from "./api"
 console.log("环境变量  => ", import.meta.env);
 const app = createApp(App);
 app.config.globalProperties.$request = request;
 app.config.globalProperties.$storage = storage;
+app.config.globalProperties.$api = api;
 app.use(router).use(ElementPlus).mount("#app");
