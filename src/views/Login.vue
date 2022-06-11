200816315f510d3db65048e846b96fa83700218b
vinkim <928287706@qq.com>
2022-06-11 18:20:50 +0800

    登录页面交互功能

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,55 @@
 <script>
 export default {
   name: "Login",
+  data(){
+    return{
+      user:{
+        userName:"",
+        userPwd:""
+      },
+      rules:{
+        userName:[
+          {
+            required:true,message:'请输入用户名',trigger:'blur'
+          }
+        ],
+        userPwd:[
+          {
+            required:true,message:'请输入密码',trigger:'blur'
+          }
+        ]
+      }
+    }
+  },
+  methods:{
+    login(){
+      // 判断用户是否输入
+      this.$refs.userForm.validate((valid)=>{
+        if(valid){ //如果输入
+          this.$api.login(this.user).then(res=>{
+            this.$store.commit("saveUserInfo", res);
+            this.$router.push('/welcome');
+          })
+        }
+      })
+    }
+  }
 };
 </script>
 
 <template>
   <div class="login-wrapper">
     <div class="model">
-      <el-form>
+      <el-form :model="user" status-icon :rules="rules" ref="userForm">
         <div class="title">火星</div>
-        <el-form-item>
-          <el-input type="text" prefix-icon="el-icon-user"></el-input>
+        <el-form-item prop="userName">
+          <el-input type="text" prefix-icon="el-icon-user" v-model="user.userName"></el-input>
+        </el-form-item>
+        <el-form-item prop="userPwd">
+          <el-input type="password" prefix-icon="el-icon-view" v-model="user.userPwd"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-input type="password" prefix-icon="el-icon-view"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" class="btn-login">登录</el-button>
+          <el-button type="primary" class="btn-login" @click="login">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
